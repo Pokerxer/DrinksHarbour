ea250abfc0ad43e5907fba690cf4c208b66263ca
System Administrator <root@MACs-MacBook-Pro.local>
2026-02-21 22:54:08 +0100

    fix: resolve Next.js 15 PageProps type error
    - Fix params type to be Promise<{ slug: string }> in products edit page
    - Fix params type in sub-products edit page
    - Add eslint-disable for unused vars

--- client/apps/isomorphic/src/app/(hydrogen)/ecommerce/products/[slug]/edit/page.tsx
+++ client/apps/isomorphic/src/app/(hydrogen)/ecommerce/products/[slug]/edit/page.tsx
@@ -1,8 +1,9 @@
 // @ts-nocheck
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import CreateEditProduct from '@/app/shared/ecommerce/product/create-edit';
 
-export default async function EditProductPage({ params }: { params: { slug: string } }) {
-  const slug = (await params).slug;
+export default async function EditProductPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
 
   return <CreateEditProduct slug={slug} id={slug} />;
 }
--- client/apps/isomorphic/src/app/(hydrogen)/ecommerce/sub-products/[slug]/edit/page.tsx
+++ client/apps/isomorphic/src/app/(hydrogen)/ecommerce/sub-products/[slug]/edit/page.tsx
@@ -1,8 +1,9 @@
 // @ts-nocheck
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import SubProductCreateEdit from '@/app/shared/ecommerce/sub-product/create-edit';
 
-export default async function EditSubProductPage({ params }: { params: { slug: string } }) {
-  const slug = (await params).slug;
+export default async function EditSubProductPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
 
   return <SubProductCreateEdit slug={slug} id={slug} />;
 }
